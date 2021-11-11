a9315cbbb5f65d15ccb2814a619eedee36c53fbb
Andrew Ealovega <Andrew@Ealovega.dev>
2021-11-11 17:45:17 -0500

    Add write fn

--- src/serialcxx/src/lib.rs
+++ src/serialcxx/src/lib.rs
@@ -4,10 +4,28 @@ mod serial;
 use serial::*;
 
 #[cxx::bridge(namespace="serialcxx")]
-mod ffi {
+pub mod ffi {
+
+    pub enum SerialError {
+        /// The operation succeeded.
+        NoErr = 0,
+        /// The operation was interrupted, but did not fail. Can be started again.
+        Interrupted,
+        /// Uncategorized error.
+        Other
+    }
+
     extern "Rust" {
         type Serial;
 
+        /// Attempts to write the entire buffer of bytes to the serial device.
+        ///
+        /// Errors
+        /// ------
+        ///
+        /// - Interrupted - The device transfer was interrupted. You may retry this transfer.
+        /// - Other - Any other kind of device failure, such as a disconnect.
+        fn write(self: &mut Serial, data: &[u8]) -> SerialError;
         fn open_port(path: &str, baud: u32) -> Result<Box<Serial>>;
     }
 }
--- src/serialcxx/src/serial.rs
+++ src/serialcxx/src/serial.rs
@@ -1,5 +1,6 @@
-use serialport::Result;
-use serialport::SerialPort;
+use serialport::{Result, SerialPort};
+use std::io::{ErrorKind, Write};
+use crate::ffi::SerialError;
 
 pub struct Serial {
     raw_port: Box<dyn SerialPort>,
@@ -10,6 +11,25 @@ impl Serial {
         Ok(Serial {
             raw_port: serialport::new(path, baud).open()?,
         })
+    }
+
+    /// Attempts to write the entire buffer of bytes to the serial device.
+    ///
+    /// Errors
+    /// ------
+    ///
+    /// - Interrupted - The device transfer was interrupted. You may retry this transfer.
+    /// - Other - Any other kind of device failure, such as a disconnect.
+    pub fn write(&mut self, data: &[u8]) -> SerialError {
+        let res = self.raw_port.write_all(data);
+
+        match res {
+            Ok(_) => SerialError::NoErr,
+            Err(err) => match err.kind() {
+                ErrorKind::Interrupted => SerialError::Interrupted,
+                _ => SerialError::Other,
+            },
+        }
     }
 
     pub fn callback(&self, fnc: extern "C" fn(input: u32) -> u32) {
