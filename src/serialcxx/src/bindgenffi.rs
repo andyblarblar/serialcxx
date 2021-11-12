aab44968bdf75108571d2e088b5852c399d5ac07
Andrew Ealovega <Andrew@Ealovega.dev>
2021-11-12 01:10:52 -0500

    WIP add correct C callback. Makes use of the void * user_data pattern.

--- src/serialcxx/src/bindgenffi.rs
+++ src/serialcxx/src/bindgenffi.rs
@@ -1,9 +1,11 @@
+use std::ffi::c_void;
+use std::os::raw::c_char;
 use crate::serial::Serial;
 
 #[no_mangle]
 pub unsafe extern "C" fn add_read_callback(
     serial: *mut Serial,
-    call: extern "C" fn(input: u32) -> u32,
+    user_data: *mut c_void, //Pointer to class instance to invoke with
+    call: extern "C" fn(user_data: *mut c_void, string_read: *const c_char, str_size: usize), //Pass the above user data in with every call, as that void* is the this ptr. C cannot handle clases, so C++ must define a static wrapper to use with call here that will deref the passed userdata as a this* in order to call the member function. We dont need to do anything in rust other than pass in user data
 ) {
-    (*serial).callback(call);
 }
